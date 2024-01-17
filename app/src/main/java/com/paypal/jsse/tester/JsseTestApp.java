5488e6869c7cf455db93d5678efde0fde8c922ba
RADHAKRISHNAN <radhakrishnan@paypal.com>
2024-01-17 20:45:52 +0530

    simple reproducer

--- app/src/main/java/com/paypal/jsse/tester/JsseTestApp.java
+++ app/src/main/java/com/paypal/jsse/tester/JsseTestApp.java
@@ -2,52 +2,12 @@ package com.paypal.jsse.tester;
 
 
 import com.paypal.jsse.tester.tests.jmh.JmhExecutor;
-import com.paypal.jsse.tester.tests.lnp.HttpsCallLoadExecutor;
-import com.paypal.jsse.tester.tests.sslr.SslResumptionValidator;
-import com.paypal.jsse.tester.server.ReactorNettyStandaloneServer;
-
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.function.Supplier;
-
-import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class JsseTestApp {
 
     public static void main(String[] args) {
-        final String testModeValue = System.getProperty("test.mode");
-        if(isBlank(testModeValue)) {
-            throw new RuntimeException("System property test.mode is mandatory.");
-        }
-        TestMode.getTestMode(testModeValue)
-                .orElseThrow(() -> new RuntimeException("Invalid test mode: " + testModeValue))
-                .getImplementation()
-                .get();
-    }
-
-    public enum TestMode {
-        JMH_CLIENT_CALLS("jmh-client-calls", JmhExecutor::new),
-        START_SERVER("start-server", ReactorNettyStandaloneServer::new),
-        CLIENT_LNP("client-lnp", HttpsCallLoadExecutor::new),
-        SSLR("sslr", SslResumptionValidator::new);
-
-        private final String testMode;
-        private final Supplier<?> implementation;
-
-        TestMode(final String testMode, final Supplier<?> implementation) {
-            this.testMode = testMode;
-            this.implementation = implementation;
-        }
-
-        public Supplier<?> getImplementation() {
-            return implementation;
-        }
-
-        static Optional<TestMode> getTestMode(final String testMode) {
-            return Arrays.stream(TestMode.values())
-                    .filter(mode -> mode.testMode.equalsIgnoreCase(testMode))
-                    .findFirst();
-        }
+        JmhExecutor jmhExecutor = new JmhExecutor();
+        System.out.println("Execution done");
     }
 
 }
--- app/src/main/java/com/paypal/jsse/tester/tests/jmh/HttpsCallBenchmark.java
+++ app/src/main/java/com/paypal/jsse/tester/tests/jmh/HttpsCallBenchmark.java
@@ -1,38 +1,91 @@
 package com.paypal.jsse.tester.tests.jmh;
 
-import com.paypal.jsse.tester.tests.TestExecutor;
-import com.paypal.jsse.tester.client.HttpsClient;
-import com.paypal.jsse.tester.server.HttpsServer;
+import com.paypal.jsse.test.ssl.MockSSLContextFactory;
+import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.IdentityCipherSuiteFilter;
+import io.netty.handler.ssl.JdkSslContext;
+import io.netty.handler.ssl.SslContext;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.resources.ConnectionProvider;
+import reactor.netty.tcp.SslProvider;
+
+import javax.net.ssl.SSLContext;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
 
 @State(Scope.Benchmark)
-public class HttpsCallBenchmark implements TestExecutor {
-    private HttpsClient<?> client;
-    private HttpsServer<?> server;
-    private JMHMetricsRegistry metricsRegistry;
+public class HttpsCallBenchmark {
 
+    private static final Logger logger = LoggerFactory.getLogger(HttpsCallBenchmark.class);
+
+    private HttpClient client;
+    private DisposableServer server;
+
+    private static AtomicLong servedRequests = new AtomicLong(0l);
+
+    private SslContext nettySslContext(final SSLContext sslContext, boolean isClient) {
+        return new JdkSslContext(
+                sslContext,
+                isClient,
+                null,
+                IdentityCipherSuiteFilter.INSTANCE,
+                null,
+                ClientAuth.NONE, // this value is required for the constructor but is not used for clients
+                null,
+                false);
+    }
     @Setup
-    public void setup(final JMHMetricsRegistry metricsRegistry) {
-        this.server = initializeTestServer().orElse(null);
-        this.metricsRegistry = metricsRegistry;
-        client = initializeTestClient(this.metricsRegistry, false);
+    public void setup() {
+        MockSSLContextFactory factory = new MockSSLContextFactory();
+        this.server = HttpServer
+                .create()
+                .port(6443)
+                .secure(SslProvider.builder().sslContext(nettySslContext(factory.sslContext(false), false)).build())
+                .route(routes -> routes
+                        .get("/hi",
+                                (req, res) -> {
+                                    servedRequests.incrementAndGet();
+                                    return res.sendString(Mono.just("Hello world").delayElement(Duration.ofMillis(100)));
+                                }))
+                .bindNow();
+        client = HttpClient.create(ConnectionProvider.newConnection())
+                .keepAlive(false)
+                .secure(SslProvider.builder().sslContext(nettySslContext(factory.sslContext(true), true)).build())
+                .baseUrl(String.format("https://%s:%s", "localhost", "6443"));
     }
 
     @Benchmark
     public void execute() {
-        client.executeHttpsCall();
+        //logger.info("execute : " + servedRequests.get());
+        final String response = client
+                .get()
+                .uri("/hi")
+                .responseSingle((resp, bytes) -> bytes.asString())
+                .doOnError(throwable -> logger.debug(throwable.getMessage(), throwable))
+                .onErrorReturn("failed")
+                .block();
+        if(logger.isDebugEnabled()) {
+            logger.debug("Response: " + response);
+        }
     }
 
     @TearDown
     public void cleanup() {
         if(this.server != null) {
-            this.server.stopServer();
+            this.server.disposeNow();
         }
-        this.metricsRegistry.report();
+
+        logger.info("total requests served : " + servedRequests.get());
     }
 
 }
